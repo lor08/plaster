6631f83c97014a9ce101be9a3f53a2b483b62680
KNSM <knsmrt5k@gmail.com>
2021-07-22 09:06:22 +0300

    -Добавил все оставшиеся секции на главную + хедер (4h)

--- app/js/main.js
+++ app/js/main.js
@@ -17,6 +17,39 @@ $(document).ready(function () {
                     nextArrow: $(this).find('.slider-button-next'),
                 });
             }
+
+            if ($(this).length && $(this).hasClass('slider-video')) {
+                sliderWrapper.slick({
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '60px',
+                    prevArrow: $(this).find('.slider-button-prev'),
+                    nextArrow: $(this).find('.slider-button-next'),
+                });
+            }
+
+            if ($(this).length && $(this).hasClass('slider-demo')) {
+                sliderWrapper.slick({
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false,
+                    asNavFor: $(this).find('.slider__content .item__wrapper'),
+                });
+
+                $(this).find('.slider__content .item__wrapper').slick({
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: true,
+                    asNavFor: sliderWrapper,
+                    prevArrow: $(this).find('.slider-arrow-prev'),
+                    nextArrow: $(this).find('.slider-arrow-next'),
+                });
+            }
         });
     });
 
